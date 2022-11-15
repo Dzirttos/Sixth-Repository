218535d80db803380affa8c1ee84db6e96430dca
Dizrtt <demonic2003@bk.ru>
2022-11-15 23:55:49 +0200

    Task 41 solved.

--- HW1_Task41/Program.cs
+++ HW1_Task41/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+Console.Clear();
+
+int[] GetArrayFromString(string stringarray)
+{
+    string[] nums = stringarray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] result = new int[nums.Length];
+    for (int i = 0; i < nums.Length; i++)
+    {
+        result[i] = Convert.ToInt32(nums[i]);
+    }
+    return result;
+}
+
+Console.WriteLine("Please enter any amount of numbers via space (use spacebar):");
+string numbers = Console.ReadLine();
+int[] array = GetArrayFromString(numbers);
+int count = 0;
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.Write("The numbers above 0 are: ");
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+        Console.Write($"{array[i]} ");
+    }
+}
+Console.ResetColor();
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($"You have entered {count} numbers which are bigger than 0.");
